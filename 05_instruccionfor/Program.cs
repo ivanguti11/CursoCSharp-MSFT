d6d5b714fffe43470c7d6612f453f0f16f81f041
Ivan Gutierrez <ivan.guti.11@hotmail.com>
2026-07-27 23:39:47 -0600

    fizzbuzz

---
+++ 05_instruccionfor/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+for (int i = 0; i < 10; i += 3)
+{
+    Console.WriteLine(i);
+}
+
+for (int i = 10; i >= 0; i--)
+{
+    Console.WriteLine(i);
+}
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 7) break;
+}
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = 0; i < names.Length; i++)
+    if (names[i] == "David") names[i] = "Sammy";
+
+foreach (var name in names) Console.WriteLine(name);
+*/
+
+
+using System.Threading.Tasks.Dataflow;
+
+string fizz = "Fizz";
+string buzz = "Buzz";
+
+for (int i = 1; i <= 100; i++)
+{
+    if(i % 3 == 0)
+        if (i % 5 ==0)
+        Console.WriteLine($"{i} - {fizz}{buzz}");
+        else 
+        Console.WriteLine($"{i} - {fizz}");
+    else if (i % 5 == 0)
+    Console.WriteLine($"{i} - {buzz}");
+    else
+    Console.WriteLine(i);
+}
---
+++ 05_switchCase/05_switchCase.csproj/Program.cs
@@ -0,0 +1,56 @@
+﻿// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+using System.Threading.Tasks.Dataflow;
+
+string sku = "03-BL-S";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch(product[0])
+{
+    case "01":
+        type = "Sweat shirt";
+    break;
+    case "02":
+    type = "T-Shirt";
+    break;
+    case "03":
+    type = "Sweat pants";
+    break;
+    default:
+        type = "Other";
+    break;
+}
+switch(product[1])
+{
+    case "BL":
+    color = "Negro";
+    break;
+    case "MN":
+    color = "Marron";
+    break;
+    default:
+    color = "Blanco";
+    break;
+}
+switch(product[2])
+{
+    case "S":
+    size = "Chico";
+    break;
+    case "M":
+    size = "Mediana";
+    break;
+    case "L":
+    size = "Largo";
+    break;
+    default:
+    size = "one size fit all";
+    break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
